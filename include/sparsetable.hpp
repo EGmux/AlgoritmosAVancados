08fb3956af33aa27b9fb86b3503270b745464255
Enzo Bissoli <enzogurgel15@gmail.com>
2023-04-21 00:51:50 +0000

    fix: applies clang-tidy/format improves code readability

--- include/sparsetable.hpp
+++ include/sparsetable.hpp
@@ -1,40 +1,46 @@
 #ifndef SPARSE_TABLE_H
 #define SPARSE_TABLE_H
 
-#include <vector>
 #include <cstdint>
+#include <vector>
 
-typedef std::vector<std::vector<uint32_t>> mat_uint32_t;
+typedef std::vector<std::vector<uint32_t>> MatUint32T;
 
-class sparseTable{
-  public:
-  /* 
-   *  Mutable method that given two valid indexes reconstruct partially the sparse table.
-   *  @params cur_index The index that will be updated in the array
-   *  @params updated_index The updated value of the targeted index
-   *  @returns None
-   */
-    void upd(const uint32_t cur_index, const uint32_t updated_index);
+class SparseTable {
+public:
+    /*
+     *  Mutable method that given two valid indexes reconstruct partially the sparse table.
+     *  @params cur_index The index that will be updated in the array
+     *  @params updated_index The updated value of the targeted index
+     *  @returns None
+     */
+    void Upd( uint32_t curIndex,  uint32_t updatedIndex);
 
-  /* 
-   *  Immutable method that returns one of the following computations for the specificed range: MIN, MAX, SUM.
-   *  @params lower_index The start of the range query
-   *  @params high_index The end of the range query
-   *  @returns The computation result
-   */
-    uint32_t query(const uint32_t low_index, const uint32_t high_index);
-  
- /* 
-  * Default constructor that construct the sparse table
-  * @params initialArr The vector that will generate 
-  * the sparse table
-  * @parmas op The operation computed for each index of the table, can be SUM, MIN or MAX
-  * @return None
-  */
-    sparseTable(const std::vector<uint32_t> initialArr, uint32_t(*op)(const uint32_t, const uint32_t));
+    /*
+     *  Immutable method that returns one of the following computations for the specificed range:
+     * MIN, MAX, SUM.
+     *  @params lower_index The start of the range query
+     *  @params high_index The end of the range query
+     *  @returns The computation result
+     */
+    uint32_t Query( uint32_t lowIndex,  uint32_t highIndex);
 
-    private:
-    mat_uint32_t sparseTableData;
+    /*
+     * Default constructor that construct the sparse table
+     * @params initialArr The vector that will generate
+     * the sparse table
+     * @parmas op The operation computed for each index of the table, can be SUM, MIN or MAX
+     * @return None
+     */
+    SparseTable(const std::vector<uint32_t> &initialArr,
+                uint32_t (*op)(const uint32_t, const uint32_t));
+
+private:
+    MatUint32T m_sparseTableData;
+    uint32_t (*m_op)(
+        const uint32_t,
+        const uint32_t);    // op is the name of the attribute,
+                            // https://stackoverflow.com/questions/12772372/c-class-that-has-a-pointer-to-a-function-as-an-attribute
 };
 
-#endif  // SPARSE_TABLE_H
+#endif    // SPARSE_TABLE_H
--- src/sparsetable.cpp
+++ src/sparsetable.cpp
@@ -1,18 +1,19 @@
-#include "sparsetable.hpp"
-
-#include "include/sparsetable.hpp"
+#include "../include/sparsetable.hpp"
+#include <algorithm>
 #include <cmath>
-
+#include <cstdint>
+#include <array>
 /*
 * first we generate the zeroth row, the intialArr data
 * second we apply dynamic programming to fill each new row
 * each row is filled with a computation that require log2(2^[curRow]) elements to compute the result. 
 * we can fill the sparsetable, thus the number of total num of rows are floor(log2(initialArr.size()))
 */
-sparseTable::sparseTable(const std::vector<uint32_t> intialArr, uint32_t(*op)(const uint32_t, const uint32_t) )
+SparseTable::SparseTable(const std::vector<uint32_t>& intialArr, uint32_t(*op)(const uint32_t, const uint32_t) ):m_op{}
 {
     const uint32_t numRows = floor(log2(intialArr.size())) + 1;
-    std::copy(intialArr.begin(), intialArr.end(), sparseTableData[0]); // fills zeroth row
+    m_sparseTableData.reserve((floor(log2(intialArr.size()+1) + intialArr.size()))); // prealloc the matrix
+    std::copy(intialArr.begin(), intialArr.end(), m_sparseTableData[0]); // fills zeroth row
     for(uint32_t curRow{1}; curRow <= numRows; ++curRow){
         /*
         * briefly to compute for instance a index of the first row, each computation require two elements, we would do the following
@@ -23,15 +24,40 @@ sparseTable::sparseTable(const std::vector<uint32_t> intialArr, uint32_t(*op)(co
         * notice the gap of 1 index, we increase this gap to 1 << (curRow -1) for the nth row.
         */
         for(uint32_t curCol{0}; curCol + (curCol << 1) <=  intialArr.size(); ++curCol){
-            sparseTableData[curRow][curCol] = op(sparseTableData[curRow - 1][curCol],sparseTableData[curRow -1][curCol + (1 << (curRow -1))]);
+            m_sparseTableData[curRow][curCol] = op(m_sparseTableData[curRow - 1][curCol],m_sparseTableData[curRow -1][curCol + (1 << (curRow -1))]);
         };
     };
 }
 
 /*
-* To split the original range in two subranges we do the following
-*  
+* We start backwards, that is first compute the largest subarray that has power of 2 and 'fits' in the range
+*  then compute the second largest subarray that still fits after you ignore the indexes that were covered by the previous one
+*   repeat until the current index is either the end of array or higher.
 */
-uint32_t sparseTable::query(const uint32_t lowIndex, const uint32_t highIndex){
-    
-}
+uint32_t SparseTable::Query( uint32_t lowIndex, const uint32_t HIGH_INDEX){
+    uint32_t curIndex;
+    uint32_t curAcc;
+    while(lowIndex < HIGH_INDEX){
+        curIndex = log2(HIGH_INDEX - lowIndex);
+        curAcc = m_op(curAcc,m_sparseTableData[curIndex][lowIndex]);
+        lowIndex += (1 << curIndex); 
+    };
+    return curAcc; 
+};
+
+/*
+* After one of the indexes in zeroth row has been updated we need to update the computations that depend on it
+* an analysis will show that it propagate similar to a rectangular triangle where the height is the  column where
+* the update happened, the next update is the below row and the imediate left index and the next row after the first 
+* increase the number of neighbours that must be updated, until the last, where all the columns to the left of the 
+* updated value are re-computed.
+*/
+void SparseTable::Upd(const uint32_t CUR_INDEX, const uint32_t UPDATED_INDEX){
+    m_sparseTableData[0][CUR_INDEX] = UPDATED_INDEX;
+    uint32_t curIter{1};
+    for(uint32_t curRow{1}; curRow < m_sparseTableData.size(); ++curRow){
+        for(uint32_t curCol{static_cast<uint32_t>(std::max(0,static_cast<int>(curIter) - (1 << curIter) -1))}; curCol < CUR_INDEX; ++curCol){
+            m_sparseTableData[curRow][curCol] = m_op(m_sparseTableData[curRow - 1][curCol],m_sparseTableData[curRow -1][curCol + (1 << (curRow -1))]);
+        };
+    };       
+};
