42e703410b8f6b2d3e5599a88b6a2bc19e44d621
Enzo Bissoli <enzogurgel15@gmail.com>
2023-04-26 23:40:55 +0000

    add: Declaration based on lista6 notes skippedlists a struct with (key,val), Insertion, Removal and Search operations also each node has a vector of linkedlists, where each index is a linked list with different height

--- include/includeSkipList.hpp
+++ include/includeSkipList.hpp
@@ -1,18 +1,23 @@
 #ifndef SKIP_LIST_H
 #define SKIP_LIST_H
 
-#include <forward_list> // single linked list
+#include <forward_list>
 #include <cstdint>
 #include <vector>
 
-typedef std::pair<std::forward_list<uint32_t>*,std::vector<std::forward_list<uint32_t>*>> USkiplist;
+struct USkipList{
+    uint32_t m_key;
+    uint32_t m_val;
+    uint32_t m_height;
+    std::vector<USkipList*> m_nodes; // each pointer points to the next element in their respective height
 
-struct SkipList{
-    bool Insertion( uint32_t valueToInsert);
-    bool Removal( uint32_t valueToRemove);
-    std::pair<uint32_t, uint32_t> Find( uint32_t valueToFind);
-    SkipList();
-    USkiplist* m_sentinel{nullptr};
+    std::pair<uint32_t,uint32_t> Search(USkipList* L, uint32_t key);
+    std::vector<USkipList*> Precursors(USkipList* L, uint32_t key);
+    bool Insert(USkipList* L, uint32_t key, uint32_t val, uint32_t(*RNG)());
+    uint32_t RandomHeight(USkipList* L, uint32_t(*RNG)());
+    uint32_t MaxHeight(USkipList* L);
+    bool Delete(USkipList* L, uint32_t key);
+    USkipList();
 };
 
 
