e9dff1db92b91b16eb111248a977aac851738cf3
Enzo Bissoli <enzogurgel15@gmail.com>
2023-04-27 09:26:31 +0000

    fix: alter declaration of Insert Change the fn to pointer parameter to be called RNG

--- include/includeSkipList.hpp
+++ include/includeSkipList.hpp
@@ -7,13 +7,12 @@
 
 struct USkipList{
     uint32_t m_key;
-    uint32_t m_val;
     uint32_t m_height;
     std::vector<USkipList*> m_nodes; // each pointer points to the next element in their respective height
 
     std::pair<uint32_t,uint32_t> Search(USkipList* L, uint32_t key);
     std::vector<USkipList*> Precursors(USkipList* L, uint32_t key);
-    bool Insert(USkipList* L, uint32_t key, uint32_t val, uint32_t(*RNG)());
+    bool Insert(USkipList* L, uint32_t key, uint32_t(*)());
     uint32_t RandomHeight(USkipList* L, uint32_t(*RNG)());
     uint32_t MaxHeight(USkipList* L);
     bool Delete(USkipList* L, uint32_t key);
